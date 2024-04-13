a0553e0a46b635065a742c36306ad5e57d605096
callmecua1 <josuaoconner2321@gmail.com>
2024-04-13 11:35:38 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+const list = [
+    
+    {
+        id : 1,
+        title : "Cara Mengatasi Error pada Printer",
+    },
+
+    {
+        id : 2,
+        title : "Cara Mengetahui IP Printer HP",
+    },
+
+    {
+        id : 3,
+        title : "Cara Mengetahui IP Printer Epson",
+    },
+
+    {
+        id : 4,
+        title : "Mengatasi PC yang Tidak Bisa Hidup",
+    },
+
+    {
+        id : 5,
+        title : "Mengecek Nama Komputer melalui Pengaturan Windows",
+    },
+
+    {
+        id : 6,
+        title : "Mengecek IP pada PC/Laptop",
+    },
+
+    {
+        id : 7,
+        title : "Mengganti source Tv karena tidak ada sinyal",
+    },
+
+    {
+        id : 8,
+        title : "Recall email",
+    },
+
+    {
+        id : 9,
+        title : "Cara forget password wifi",
+    },
+
+    {
+        id : 10,
+        title : "Cara forget password wifi",
+    },
+
+    {
+        id : 11,
+        title : "Cara ticketing helpdesk",
+    }
+
+]
+
+export default list
